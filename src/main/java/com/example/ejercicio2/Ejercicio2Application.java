1523145a452ef9fd6301bd416d28818708ec81d6
bsiafas10 <brunosiafas@gmail.com>
2023-12-22 18:37:44 -0300

    Actualizar puerto para Heroku

--- src/main/java/com/example/ejercicio2/Ejercicio2Application.java
+++ src/main/java/com/example/ejercicio2/Ejercicio2Application.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ejercicio2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ejercicio2Application.class, args);
-	}
+    public static void main(String[] args) {
+        // Obtener el puerto del entorno o usar 8080 como predeterminado
+        String port = System.getenv("PORT");
+        if (port == null || port.isEmpty()) {
+            port = "8080";
+        }
 
+        // Configurar el puerto
+        System.setProperty("server.port", port);
+
+        // Iniciar la aplicación Spring Boot
+        SpringApplication.run(Ejercicio2Application.class, args);
+    }
 }
